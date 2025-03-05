eecfaf4b362ef52505861155b68e2262c0d26c90
badmoham <badmoham@users.noreply.github.com>
2025-03-05 13:16:05 +0330

    added auto migration

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"GoChat/config"
+	"GoChat/models"
 	"GoChat/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	config.DBInit()
+	err := config.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		panic("migration failed")
+	}
+
 	router := gin.Default()
 
 	routes.SetupUserRoutes(router)
